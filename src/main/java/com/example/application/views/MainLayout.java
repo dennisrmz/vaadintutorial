75f0a7ee23e7fe890b80db38a4e2aed80fbddf23
dennisrmz <orellanadennis12@gmail.com>
2022-05-15 23:24:57 -0600

    Tutorial vaadin

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -1,8 +1,10 @@
 package com.example.application.views;
 
+import com.example.application.security.SecurityService;
 import com.example.application.views.list.ListView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -11,9 +13,12 @@ import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
 
 public class MainLayout extends AppLayout {
-    
-    public MainLayout(){
-        
+
+    private SecurityService securityService;
+
+    public MainLayout(SecurityService securityService){
+        this.securityService = securityService;
+
         createHeader();
         createDrawer();
         
@@ -23,7 +28,8 @@ public class MainLayout extends AppLayout {
         H1 logo = new H1("Vaadin CRM");
         logo.addClassNames("text-l", "m-m");
 
-        HorizontalLayout header =  new HorizontalLayout(new DrawerToggle(), logo);
+        Button log_out = new Button("Log Out", e -> securityService.logout());
+        HorizontalLayout header =  new HorizontalLayout(new DrawerToggle(), logo, log_out);
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         header.expand(logo);
         header.setWidthFull();
